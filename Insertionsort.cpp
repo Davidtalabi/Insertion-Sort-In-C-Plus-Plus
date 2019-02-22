2ca31bbd8411d5544fab5ebeb6086b63c350d46a
David Talabi <Dtalabi@ucmerced.edu>
2019-02-21 20:35:08 -0800

    Update Insertionsort.cpp

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -20,23 +20,27 @@ int value,hole,i;
  }
 }
 
-void displayarray(int A[], int n)
+void displayarray(int A[], int n) //function to display array elements, will be recursively called in main function
 {
-    for(int i=0; i<n; i++)
-    cout << A[i] << ";";
+	for(int i=0; i<n; i++)
+		cout << A[i] << ";"; //displaying the array, the semi colons are so that it shows a semi colon after every number that is printed
 }
 
-int main()
+
+int main() //Main function where we recursively call the functions made earlier so that we have an output and the user also inputs the array size and elements
 {
-    int n;
-    cin >> n;
-    int A[n];
-    
-    for(int i=0; i<n; i++)
-    cin >> A[i];
-    
-    insertionsort(A,n);
-    displayarray(A,n);
-    
-    return 0;
+
+int n; //Array size	
+cout << "Enter Array Size!" << endl;
+cin >> n; //User inputs array size
+int A[n]; //Initializing array with n elements that user inputs
+
+cout << "Enter Array Elements!" << endl;
+for(int i=0; i<n; i++)
+	cin >> A[i]; //user inputs array elements
+
+	heapSort(A,n); //recursively calling heapsort function made earlier to sort the array
+	displayarray(A,n); //recursively calling display array function to display the final sorted array
+	
+return 0;
 }
